a0e3b5fcb5b9f6ec3944a04fe0d359ea89480ae7
m6hmd <85321205+m6hmd@users.noreply.github.com>
2022-04-12 06:48:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
-w = ['1', "2","3","4",'5', "6","7","8",'9', "0","q","w",'e', "r","t","y",'u', "i","o","p",'a', "s","d","f","g","h",'j', "k","l","z",'x', "c","v","b",'n', "m"]
+w = ['1', "2","3","4",'5', "6","7","8",'9', "0","a","b",'c', "d","e","f",'g', "h","i","j",'k', "l","m","n","o","p",'q', "r","s","t",'u', "v","w","x",'y', "z"]
 @bot.message_handler(content_types=["text"])
 def gen(message):
     if "/" not in message.text:
